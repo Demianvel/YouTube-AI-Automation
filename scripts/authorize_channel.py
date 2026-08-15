3e05e5514e8fa586ca8103bb1647e584122f62ab
Demianvel <149445861+Demianvel@users.noreply.github.com>
2026-08-15 00:52:51 -0300

    feat: authorize YouTube engagement comments

--- scripts/authorize_channel.py
+++ scripts/authorize_channel.py
@@ -11,6 +11,7 @@ from googleapiclient.discovery import build
 SCOPES = [
     "https://www.googleapis.com/auth/youtube.upload",
     "https://www.googleapis.com/auth/youtube.readonly",
+    "https://www.googleapis.com/auth/youtube.force-ssl",
 ]
 
 
